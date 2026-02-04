483c4a1435422df8710b8c518433ec5fff58655b
Supun Subodha <supunmalsingha@gmail.com>
2026-02-04 12:04:17 +0530

    Created method to create new database,table, and default user when user runs the program first time inside their pc

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,12 +1,16 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnection {
+    static String url;
+    static String user;
+    static String password;
     public static void databaseConnection(String databaseUsername, String databasePassword) {
-        String url = "jdbc:mysql://localhost:3306/game";
-        String user = databaseUsername;
-        String password = databasePassword;
+        url = "jdbc:mysql://localhost:3306/game";
+        user = databaseUsername;
+        password = databasePassword;
 
         System.out.println("Connecting...");
 
@@ -14,6 +18,32 @@ public class DatabaseConnection {
             System.out.println("Success! Database is connected.");
         }catch (SQLException e){
             System.out.println("Connection failed!");
+            createDatabase();//If connection failed it could be the first time this pc running the game,So first of all database and table should create.
+        }
+    }
+    public static void createDatabase(){
+        System.out.println("Creating database...");
+        try (Connection conn = DriverManager.getConnection(url,user,password);
+             Statement stmt = conn.createStatement()) {
+
+            // Create database
+            stmt.executeUpdate("CREATE DATABASE IF NOT EXISTS game");
+            System.out.println("Database created successfully.");
+
+            // Use the new database
+            stmt.executeUpdate("USE game");
+
+            // Create table(s)
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS users (" +
+                    "username varchar(50) PRIMARY KEY," +
+                    "score INT," +
+                    "level INT);";
+            stmt.executeUpdate(createTableSQL);
+            System.out.println("Tables created successfully.");
+            stmt.executeUpdate("INSERT INTO users(username,score,level) VALUES ('default',0,1);");
+            System.out.println("Created default user successfully.");
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
